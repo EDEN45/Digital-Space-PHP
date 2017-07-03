6852295fcca9bcaad330a5a2c705718f81c40a33
EDEN45 <futurionerv@outlook.com>
2017-07-03 11:58:38 +0700

    переделки

--- 02.syntax/index1.php
+++ 02.syntax/index1.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vyacheslav
- * Date: 02.07.17
- * Time: 18:06
- */
 
 $arr = [];
 
--- 02.syntax/index2.php
+++ 02.syntax/index2.php
@@ -1,24 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vyacheslav
- * Date: 02.07.17
- * Time: 18:13
- */
 
 $countRows = 9;
 $countCols = $countRows;
 
 for ($i = 1; $i <= $countRows; $i++)
 {
-    for ($j = 1; $j <= $countCols; $j++)
-    {
-        print_lighn($i, $j);
-    }
-    echo "\n\n";
+    print_lighn($i, $countCols);
 }
 
-function print_lighn (int $i, int $j)
+function print_lighn (int $i, int $countCols)
 {
-    echo str_pad($i * $j, 4, " ", STR_PAD_LEFT);
+    for ($j = 1; $j <= $countCols; $j++)
+    {
+        echo str_pad($i * $j, 3, " ", STR_PAD_LEFT);
+    }
+    echo "\n";
 }
